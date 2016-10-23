9139dab20c2a52d8da9221d691c1cc1db5c8fc92
Daniel Lebrero <dlebrero@gmail.com>
2016-10-23 04:44:46 +0200

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ig/havoc "0.1.0-SNAPSHOT"
+(defproject ig/havoc "0.1.0"
   :description "A resilience testing library for Clojure and Docker"
   :url "https://github.com/IG-Group/Havoc"
   :license {:name "Apache License 2.0"
