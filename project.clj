42f229725f7c140d58c13cb1b0a27f8705b7fece
Daniel Lebrero <dlebrero@gmail.com>
2016-11-08 22:36:56 +0100

    lein-release plugin: bumped version from 0.1.1 to 0.1.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ig/havoc "0.1.1"
+(defproject ig/havoc "0.1.2-SNAPSHOT"
   :description "A resilience testing library for Clojure and Docker"
   :url "https://github.com/IG-Group/Havoc"
   :license {:name "Apache License 2.0"
